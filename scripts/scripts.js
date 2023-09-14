011ac839343754e0d947c1ae8e230bc792db1743
fybyoyo <50716439+fybyoyo@users.noreply.github.com>
2023-09-14 14:11:29 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,16 +11,16 @@ async function readCounty(url)
   console.log(datacnty);
 
    
-  /* console.log(datacnty.name);
+   console.log(datacnty.name);
   document.getElementById('countyname').innerText=datacnty.name;
    
   for (var i=0; i<datacnty.snakeids.length; i++) {
     console.log(datacnty.snakeids[i]);
-  }
+  };
 
   for (var i=0; i<datacnty.neighborsnakeids.length; i++) {
     console.log(datacnty.neighborsnakeids[i]);
-  } */
+  };
 
 
 
